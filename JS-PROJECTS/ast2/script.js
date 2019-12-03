61247cce5455fde833da762c5e17f8a93ed5aab6
Koshal Shakya <koshal@acd.edu.np>
2019-12-03 14:29:09 +0545

    Edited

--- JS-PROJECTS/ast2/script.js
+++ JS-PROJECTS/ast2/script.js
@@ -1,5 +1,4 @@
 function Carousel(containerId) {
-
   var indicatorWrapper;
   var that = this;
 
@@ -15,123 +14,136 @@ function Carousel(containerId) {
   this.imageCount = this.images.length;
 
   // this.images = Array.from(this.imagesWrapper.getElementsByTagName('img'));
-  this.func = function () {
+  this.func = function() {
     // console.log(this.addDirection());
-  }
+  };
 
-  this.init = function () {
+  this.init = function() {
     this.setStyle();
     this.addDirection();
     this.addIndicators();
+    this.playSlide();
     this.activateIndicator(this.currentImageIndex);
+  };
 
-  }
+  this.playSlide = function() {
+    this.timer = setInterval(
+      function() {
+        if (this.currentImageIndex + 1 > this.imageCount) {
+          this.currentImageIndex = 0;
+        }
+        this.slideImage(this.currentImageIndex);
+        this.activateIndicator(this.currentImageIndex);
+        this.currentImageIndex++;
+      }.bind(this),
+      1500
+    );
+  };
 
-  this.addDirection = function () {
-    moveLeft = document.createElement('div');
-    moveRight = document.createElement('div');
+  this.addDirection = function() {
+    moveLeft = document.createElement("div");
+    moveRight = document.createElement("div");
 
-    moveLeft.classList.add('prev-img');
-    moveRight.classList.add('next-img');
+    moveLeft.classList.add("prev-img");
+    moveRight.classList.add("next-img");
 
     this.containerId.appendChild(moveLeft);
     this.containerId.appendChild(moveRight);
-    var left = document.createElement('i');
-    var right = document.createElement('i');
-    moveLeft.appendChild(left).className = "fas fa-chevron-left";
-    moveRight.appendChild(right).className = "fas fa-chevron-right";
+    var leftArrow = document.createElement("i");
+    var rightArrow = document.createElement("i");
+    moveLeft.appendChild(leftArrow).className = "fas fa-chevron-left";
+    moveRight.appendChild(rightArrow).className = "fas fa-chevron-right";
 
-    // moveLeft.addEventListener('click', console.log('object'));
+    this.left = moveLeft;
+    this.right = moveRight;
 
+    this.left.addEventListener("click", this.slideLeft.bind(this));
+    this.right.addEventListener("click", this.slideRight.bind(this));
+  };
 
+  this.slideRight = function() {
+    if (this.isCurrentImageLast()) {
+      this.currentImageIndex = 0;
+      this.slideImage(this.currentImageIndex);
+      this.activateIndicator(this.currentImageIndex);
+    } else {
+      this.currentImageIndex++;
+      this.slideImage(this.currentImageIndex);
+      this.activateIndicator(this.currentImageIndex);
+    }
+    console.log("log");
+  };
 
-  }
+  this.slideLeft = function() {
+    if (this.isCurrentImageFirst()) {
+      this.currentImageIndex = this.imageCount - 1;
+      this.slideImage(this.currentImageIndex);
+      this.activateIndicator(this.currentImageIndex);
+    } else {
+      this.currentImageIndex--;
+      this.slideImage(this.currentImageIndex);
+      this.activateIndicator(this.currentImageIndex);
+    }
+  };
 
+  this.slideImage = function(c) {
+    this.imagesWrapper[0].style.marginLeft = "-" + c * this.width + "px";
+  };
 
-
-  // this.slideRight = function (c) {
-  // if (this.isCurrentImageLast()) {
-  //   this.currentImageIndex = 0;
-  //   this.slideImage(this.currentImageIndex);
-  //   this.activateIndicator(this.currentImageIndex);
-  // } 
-  // else {
-  //   this.currentImageIndex++;
-  //   this.slideImage(this.currentImageIndex);
-  //   this.activateIndicator(this.currentImageIndex);
-  // }
-
-  // this.slideLeft = function () {
-  //   if (this.isCurrentImageFirst()) {
-  //     this.currentImageIndex = this.imageCount - 1;
-  //     this.slideImage(this.currentImageIndex);
-  //     this.activateIndicator(this.currentImageIndex);
-  //   } else {
-  //       this.currentImageIndex--;
-  //     this.slideImage(this.currentImageIndex);
-  //     this.activateIndicator(this.currentImageIndex);
-  //   }
-  // }
-
-  this.slideImage = function (c) {
-    this.imagesWrapper[0].style.marginLeft = '-' + c * this.width + 'px';
-  }
-
-  this.addIndicators = function () {
-    indicatorWrapper = document.createElement('div');
-    indicatorWrapper.classList.add('dots-wrapper');
+  this.addIndicators = function() {
+    indicatorWrapper = document.createElement("div");
+    indicatorWrapper.classList.add("dots-wrapper");
     this.containerId.appendChild(indicatorWrapper);
     this.addDots();
-  }
+  };
 
-  this.addDots = function () {
+  this.addDots = function() {
     for (let i = 0; i < this.imageCount; i++) {
       var dot = document.createElement("span");
-      dot.classList.add('dots');
+      dot.classList.add("dots");
       indicatorWrapper.appendChild(dot);
-      dot.addEventListener("click", function () {
+      dot.addEventListener("click", function() {
         currentImageIndex = i;
         that.slideImage(currentImageIndex);
-        that.activateIndicator(currentImageIndex)
+        that.activateIndicator(currentImageIndex);
       });
     }
-  }
+  };
 
-  this.setStyle = function () {
-    this.containerId.style.width = this.width + 'px';
-    this.containerId.style.height = this.height + 'px';
-    this.containerId.style.position = 'relative';
-    this.containerId.style.overflow = 'hidden';
+  this.setStyle = function() {
+    this.containerId.style.width = this.width + "px";
+    this.containerId.style.height = this.height + "px";
+    this.containerId.style.position = "relative";
+    this.containerId.style.overflow = "hidden";
 
-    this.imagesWrapper[0].style.width = this.imageCount * this.width + 'px';
+    this.imagesWrapper[0].style.width = this.imageCount * this.width + "px";
 
     for (let i = 0; i < this.imageCount; i++) {
-      this.images[i].style.float = 'left';
+      this.images[i].style.float = "left";
     }
-  }
+  };
 
-  this.activateIndicator = function (c) {
-    var active = document.querySelector('.active');
-    var dots = Array.from(this.containerId.getElementsByClassName('dots'));
+  this.activateIndicator = function(c) {
+    var active = document.querySelector(".active");
+    var dots = Array.from(this.containerId.getElementsByClassName("dots"));
     if (active != null) {
-      active.classList.remove('active');
+      active.classList.remove("active");
     }
-    dots[c].classList.add('active');
+    dots[c].classList.add("active");
     this.currentImageIndex = c;
-  }
+  };
 
-  this.isCurrentImageLast = function () {
+  this.isCurrentImageLast = function() {
     return this.currentImageIndex == this.imageCount - 1;
-  }
+  };
 
-  this.isCurrentImageFirst = function () {
+  this.isCurrentImageFirst = function() {
     return this.currentImageIndex == 0;
-  }
+  };
 }
 
-var container = document.getElementsByClassName('carousel-container');
+var container = document.getElementsByClassName("carousel-container");
 
 var carousel = new Carousel(container);
 carousel.init();
-carousel.func();
-
+// carousel.func();
---
+++ JS-PROJECTS/ast5/js/script.js
@@ -0,0 +1,4 @@
+function Game(ctx) {}
+
+const canvas = document.getElementById("flappy-world");
+const ctx = canvas.getContext("2d");
